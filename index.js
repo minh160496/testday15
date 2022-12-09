057a4babeaa644e925ac5834c2ee4f9ea19ce9da
minhle <leminh16041996@gmail.com>
2022-12-09 07:08:51 +0700

    com

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ Bài tập
  * Bước 4: Trả về kết quả min
  */
 function minOf(arr) {
-    let min = arr[0];
-    for (let n of arr) if (n < min) min = n;
-    return min;
+  let min = arr[0];
+  for (let n of arr) if (n < min) min = n;
+  return min;
 }
 /**
  * Tính số tiền điện theo công thức tính giá điện bậc thang như sau:
@@ -47,19 +47,22 @@ function minOf(arr) {
 //  * bill(100); // 170600
  */
 function bill(kwh) {
-    const a = 1678;
-    const b = 1734;
-    const c = 2014;
-    const d = 2536;
-    const e = 2834;
-    const f = 2972;
+  const a = 1678;
+  const b = 1734;
+  const c = 2014;
+  const d = 2536;
+  const e = 2834;
+  const f = 2972;
 
-    if (kwh <= 50) return kwh * a;
-    if (kwh > 50 && kwh <= 100) return (50 * a) + (kwh - 50) * b;
-    if (kwh > 100 && kwh <= 200) return (50 * a) + 50 * b + (kwh - 100) * c;
-    if (kwh > 200 && kwh <= 300) return (50 * a) + 50 * b + 100 * c + (kwh - 200) * d;
-    if (kwh > 300 && kwh <= 400) return (50 * a) + 50 * b + 100 * c + 100 * d + (kwh - 300) * e;
-    if (kwh > 400 && kwh <= 400) return (50 * a) + 50 * b + 100 * c + 100 * d + 100 * e + (kwh - 400) * f;
+  if (kwh <= 50) return kwh * a;
+  if (kwh > 50 && kwh <= 100) return 50 * a + (kwh - 50) * b;
+  if (kwh > 100 && kwh <= 200) return 50 * a + 50 * b + (kwh - 100) * c;
+  if (kwh > 200 && kwh <= 300)
+    return 50 * a + 50 * b + 100 * c + (kwh - 200) * d;
+  if (kwh > 300 && kwh <= 400)
+    return 50 * a + 50 * b + 100 * c + 100 * d + (kwh - 300) * e;
+  if (kwh > 400 && kwh <= 400)
+    return 50 * a + 50 * b + 100 * c + 100 * d + 100 * e + (kwh - 400) * f;
 }
 
 /**
@@ -76,7 +79,7 @@ biểu thị thời gian tương ứng sau n giây
  * calcTime('20:15:45', 15); // '20:16:00'
  * calcTime('20:15:45', -46); // '20:14:59'
  */
-function calcTime(time, n) { }
+function calcTime(time, n) {}
 /**
  * Kiểm tra một chuỗi có phải đối xứng hay không (viết xuôi hay viết
 ngược đều giống nhau, không phân biệt chữ hoa chữ thường và không tính
@@ -91,23 +94,22 @@ dấu cách)
  */
 
 function isPalindrome(str) {
-    str = str.toLowerCase();
-    let arr = [];
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] != ' ')
-            arr.push(str[i]);
-    }
+  str = str.toLowerCase();
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] != " ") arr.push(str[i]);
+  }
 
-    let arr2 = []
-    for (let k in arr) {
-        arr2[k] = arr[k];
-    }
+  let arr2 = [];
+  for (let k in arr) {
+    arr2[k] = arr[k];
+  }
 
-    let newArr = arr2.reverse();
-    for (let j = 0; j < arr.length; j++) {
-        if (newArr[j] != arr[j]) return false;
-    }
-    return true;
+  let newArr = arr2.reverse();
+  for (let j = 0; j < arr.length; j++) {
+    if (newArr[j] != arr[j]) return false;
+  }
+  return true;
 }
 
 /**
@@ -124,11 +126,27 @@ function isPalindrome(str) {
  * @example
  * snail(10, 3, 1); // 5
  */
+
+//Ý TƯỞNG
+// B1; TA CỨ CHO SỐ  MÉT ĐI ĐƯỢC 1 NGÀY CỦA CON ỐC LÀ X - Y
+//B2: VÌ BAN NGÀY NẾU NÓ ĐÃ LÊN ĐƯỢC ĐẾN ĐỘ CAO H RỒI THÌ KHÔNG THỂ TRỪ LÙI ĐI QUÃNG ĐƯỢC THỤT LẠI BAN ĐÊM ĐƯỢC NÊN TA PHẢI KIỂM TRA BẦNG CÁCH (1)
+//B3: IN RA KẾT QUẢ
 function snail(h, x, y) {
-    if (h <= x) return 1;
-    return (Math.ceil(h / (x - y)))
+  if (x >= h) {
+    return 1;
+  }
+  let test = Math.ceil(h / (x - y));
+  let test2 = 0;
+  let test3 = 0;
+  for (let i = 0; i < test; i++) {
+    //(1): CÁCH KIỂM TRA XEM ĐẾN BUỔI BAN NGÀY CỦA NGÀY THỨ MẤY THÌ ỐC SÊN RA KHỎI RIẾNG
+    test2 = (test - i) * (x - y) + x;
+    if (test2 >= h) test3 = i;
+  }
+  return test - test3;
 }
 
+console.log(snail(10, 5, 4));
 /**
  * Sắp xếp các chữ số trong một số nguyên dương bất kỳ để tạo ra số nhỏ
 nhất (giữ nguyên số chữ số ban đầu, bao gồm cả số 0)
@@ -140,23 +158,29 @@ nhất (giữ nguyên số chữ số ban đầu, bao gồm cả số 0)
  * @example
  * sortNumber(5307510); // 1003557
  */
-function sortNumber(n) {
-    let nString = n.toString();
-    let result;
-    let arr = [];
-    for (let i = 0; i < nString.length; i++) {
-        arr[i] = Number.parseInt(nString[i]);
-    }
-    arr.sort();
-    if (arr[0] === 0) {
-        var note = arr[1];
-        arr[0] = note;
-        arr[1] = 0;
-        console.log(arr);
-    }
 
-    result = Number.parseInt(arr.join(''))
-    return result;
+//Ý TƯỞNG
+//B1: GÁN STRING THÀNH MẢNG
+//B2: SẮP XẾP MẢNG THEO THỨ TỰ TĂNG DẦN
+//B3: NẾU BẮT ĐẦU BẰNG SỐ 0 THÌ TÌM PHẦN TỬ CÓ GIÁ TRỊ NHỎ THỨ 2 CỦA MẢNG VÀ HOÁN ĐỔI VỊ TRÍ VỚI PHÂN TỬ CÓ GIÁ TRỊ 0
+//B3: XONG
+function sortNumber(n) {
+  let nString = n.toString();
+  let result;
+  let arr = [];
+  for (let i = 0; i < nString.length; i++) {
+    arr[i] = Number.parseInt(nString[i]);
+  }
+  arr.sort();
+  if (arr[0] === 0) {
+    var note = arr[1];
+    arr[0] = note;
+    arr[1] = 0;
+    console.log(arr);
+  }
+
+  result = Number.parseInt(arr.join(""));
+  return result;
 }
 
 /**
@@ -176,26 +200,36 @@ trị trong mảng và value là số lần xuất hiện trong mảng
  * let result = countElement(['Ba', 'Béo', 'Ba']);
  * console.log(result); // {ba: 2, béo: 1}
  */
-function countElement(arr) {
-    let obj = {}
-    let count = [];
-    let valueOf = [];
-    for (let j = 0; j < arr.length; j++) {
-        count[j] = 1;
-        for (let k = j + 1; k < arr.length; k++) {
-            if (arr[j] === arr[k] && arr[j != undefined]) {
-                count[j]++;
-                delete arr[j];
-                valueOf[j] = arr[j];
-            }
-        }
-    }
 
-    console.log(count);
-    console.log(valueOf);
+//ý TƯỞNG
+//B1: LẤY TỪNG GIÁ TRỊ CỦA MẢNG SO SÁNH VỚI CÁC PHẦN TỬ CÒN LẠI, SO SÁNH ĐẾN ĐÂU THÌ GÁN CHO PHẦN TỬ ĐÓ GIÁ TRỊ undefined
+//B2: TẠO RA MẢNG COUNT LƯU SỐ LẦN XUẤT HIỆN
+//      TẠO MẢNG VALUEOF LƯU CÁC PHẦN TỬ KHÔNG BỊ LẶP LẠI CỦA MẢNG
+//B3; OBJECT CUỐI CÙNG CÓ KEY LÀ PHẦN TỬ THUỘC VALUEOF VÀ VALUE LÀ PHẦN TỬ THUỘC COUNT VỚI INDEX GIỐNG NHAU
+function countElement(arr) {
+  let obj = {};
+  let count = [];
+  let valueOf = [];
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] != undefined) {
+      count[j] = 1;
+      valueOf[j] = arr[j];
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[j] === arr[k]) {
+          count[j]++;
+          delete arr[j];
+          delete arr[k];
+        }
+      }
+    }
+  }
+  for (let i = 0; i < count.length; i++) {
+    obj[valueOf[i]] = count[i];
+  }
+  return obj;
 }
 
-countElement(['Ba', 'Béo', 'Ba'])
+countElement(["Ba", "Béo", "Ba"]);
 /**
  * Object chứa thông tin về sinh viên
  *
@@ -257,32 +291,29 @@ ngày sinh tăng dần
  * ]
  */
 function sortStudents(students) {
-
-    //coppy mảng
-    let newStudents = [];
-    for (let i = 0; i < students.length; i++) {
-        newStudents[i] = students[i]
+  //coppy mảng
+  let newStudents = [];
+  for (let i = 0; i < students.length; i++) {
+    newStudents[i] = students[i];
+  }
+  //sort theo tên
+  newStudents.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
     }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
 
-    //sort theo điểm
-    newStudents.sort((a, b) => b.value - a.value);
+  //sort theo ngày sinh
+  newStudents.sort((a, b) => b.dateOfBirth - a.dateOfBirth);
 
-    //sort theo ngày sinh
-    newStudents.sort((a, b) => a.dateOfBirth - b.dateOfBirth);
+  //sort theo điểm
+  newStudents.sort((a, b) => b.grade - a.grade);
 
-    //sort theo tên
-    newStudents.sort((a, b) => {
-        const nameA = a.name.toUpperCase();
-        const nameB = b.name.toUpperCase();
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }
-        return 0;
-    });
-    return newStudents;
+  return newStudents;
 }
-
-
